854c03c6c411be0880c076bef21047bdc6f71e79
Andrew Ligon <andrew.ligon@nextthought.com>
2017-11-03 18:10:28 +0700

    oops

--- test/app/index.js
+++ test/app/index.js
@@ -3,11 +3,11 @@ import ReactDom from 'react-dom';
 
 import {Router, LinkTo, Route} from '../../src';
 
-function Links () {
+function Root () {
 	return (
 		<div>
-			<LinkTo.Path to="/a">A</LinkTo.Path>
-			<LinkTo.Path to="/b">B</LinkTo.Path>
+			<LinkTo.Path to="/sub">Sub</LinkTo.Path>
+			<h1>Root</h1>
 		</div>
 	);
 }
@@ -15,24 +15,46 @@ function Links () {
 function CmpA () {
 	return (
 		<div>
-			<Links />
-			<span>Component A</span>
+			<LinkTo.Name name="root">Root</LinkTo.Name>
+			<LinkTo.Name name="sub-root">Sub Root</LinkTo.Name>
+
+			<h2>Nested A</h2>
 		</div>
 	);
 }
+
 
 function CmpB () {
 	return (
 		<div>
-			<Links />
-			<span>Component B</span>
+			<LinkTo.Name name="root">Root</LinkTo.Name>
+			<LinkTo.Name name="sub-root">Sub Root</LinkTo.Name>
+
+			<h2>Nested B</h2>
 		</div>
 	);
 }
 
-const Test = Router.for(
+function SubCmp () {
+	return (
+		<div>
+			<LinkTo.Path to="a">A</LinkTo.Path>
+			<LinkTo.Path to="b">B</LinkTo.Path>
+
+			<h2>Sub Root</h2>
+		</div>
+	);
+}
+
+const Sub = Router.for(
 	Route({path: '/a', component: CmpA}),
-	Route({path: '/b', component: CmpB})
+	Route({path: '/b', component: CmpB}),
+	Route({path: '/', component: SubCmp})
+);
+
+const Test = Router.for(
+	Route({path: '/sub', component: Sub}),
+	Route({path: '/', component: Root, name: 'root'})
 );
 
 ReactDom.render(
