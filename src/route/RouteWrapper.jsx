badae78936eb14f651caef7d922598e6281adfcc
Andrew Ligon <andrew.ligon@nextthought.com>
2018-01-08 14:38:34 -0600

    NTI-4712 in the route wrapper set the match url to the baseroute

--- src/route/RouteWrapper.jsx
+++ src/route/RouteWrapper.jsx
@@ -3,28 +3,58 @@ import PropTypes from 'prop-types';
 
 import {getParamProps, isFrameless} from './utils';
 
-RouteWrapper.propTypes = {
-	routeProps: PropTypes.object,
-	componentProps: PropTypes.object,
-	routerProps: PropTypes.object,//extra props given to the router that we are passing a long
+export default class RouteWrapper extends React.Component {
+	static propTypes = {
+		routeProps: PropTypes.object,
+		componentProps: PropTypes.object,
+		routerProps: PropTypes.object,//extra props given to the router that we are passing a long
 
-	component: PropTypes.any,
-	frame: PropTypes.oneOfType([
-		PropTypes.element,
-		PropTypes.func
-	]),
+		component: PropTypes.any,
+		frame: PropTypes.oneOfType([
+			PropTypes.element,
+			PropTypes.func
+		]),
 
-	frameless: PropTypes.bool
-};
-export default function RouteWrapper ({routeProps, routerProps, componentProps, component:Component, frame:Frame, frameless}) {
-	const params = getParamProps(routeProps);
-	const component = (<Component {...routeProps} {...routerProps} {...componentProps} {...params} />);
+		frameless: PropTypes.bool
+	}
 
-	return isFrameless(Frame, frameless) ?
-		component :
-		(
-			<Frame {...routeProps} {...routerProps} {...params} frameless={frameless}>
-				{component}
-			</Frame>
-		);
+	static contextTypes = {
+		router: PropTypes.object
+	}
+
+	static childContextTypes = {
+		router: PropTypes.shape({
+			baseroute: PropTypes.string
+		})
+	}
+
+
+	getChildContext () {
+		const {router} = this.context;
+		const {routeProps} = this.props;
+		const {match} = routeProps || {};
+
+		return {
+			router: {
+				...router,
+				baseroute: match ? match.url : router.baseroute
+			}
+		};
+	}
+
+
+	render () {
+		const {routeProps, routerProps, componentProps, component:Component, frame: Frame, frameless} = this.props;
+		const params = getParamProps(routerProps);
+		const component = (<Component {...routeProps} {...routerProps} {...componentProps} {...params} />);
+
+		return isFrameless(Frame, frameless) ?
+			component :
+			(
+				<Frame {...routeProps} {...routerProps} {...params} frameless={frameless}>
+					{component}
+				</Frame>
+			);
+	}
+
 }
