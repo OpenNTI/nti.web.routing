3b96e271abcc9f2b09cfc66577a1271856e38134
Andrew Ligon <andrew.ligon@nextthought.com>
2018-04-03 11:27:15 -0500

    Use released version of NavLink for the fork

--- src/fork/NavLink.jsx
+++ src/fork/NavLink.jsx
@@ -47,13 +47,9 @@ export default function NavLink (props) {
 		...rest
 	} = props;
 
-	const path = typeof to === 'object' ? to.pathname : to;
-	// Regex taken from: https://github.com/pillarjs/path-to-regexp/blob/master/index.js#L202
-	const escapedPath = path && path.replace(/([.+*?=^!:${}()[\]|/\\])/g, '\\$1');
-
 	return (
 		<Route
-			path={escapedPath}
+			path={typeof to === 'object' ? to.pathname : to}
 			exact={exact}
 			strict={strict}
 			location={location}
