a7bc58756938430ee2bad248cb7e21b4517dc083
Ray Hatfield <ray.hatfield@nextthought.com>
2018-10-10 15:49:09 -0500

    export prompt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export getHistory from './history';
 export * as LinkTo from './link-to';
 export * as Matches from './matches';
+export {Prompt} from 'react-router';
 export Route from './route';
 export Router from './router';
 export * as View from './view';
