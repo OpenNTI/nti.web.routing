c8548fe93441246ac66af83a3f8eb5994fe400d6
Andrew Ligon <andrew.ligon@nextthought.com>
2020-06-30 12:41:40 -0500

    expose a hook to trigger user route confirmations

--- src/history/index.js
+++ src/history/index.js
@@ -4,22 +4,33 @@ import {getPrompt} from '../prompt/Store';
 
 let history;
 
+const getUserConfirmation = (_, callback) => {
+	const prompt = getPrompt();
+
+	if (prompt) {
+		return prompt(
+			x => callback(x == null ? true : x),
+			() => callback(false)
+		);
+	}
+
+	callback(true);
+};
+
 //TODO: maybe write a custom history object since this one does
 //not let you push titles on to history.
 export default function getHistory () {
+	if (!history) {
+		history = createBrowserHistory({getUserConfirmation});
+		history.awaitUserConfirmation = () => {
+			return new Promise((fulfill, reject) => (
+				getUserConfirmation(void 0, (cont) => (cont ? fulfill() : reject()))
+			));
+		};
+	}
+
 	history = history || createBrowserHistory({
-		getUserConfirmation: (_, callback) => {
-			const prompt = getPrompt();
-
-			if (prompt) {
-				return prompt(
-					x => callback(x == null ? true : x),
-					() => callback(false)
-				);
-			}
-
-			callback(true);
-		}
+		getUserConfirmation
 	});
 
 	return history;
