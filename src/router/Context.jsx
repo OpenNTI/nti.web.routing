64b1337b380ccb116e59dbeb7d75eb90774d979b
Andrew Ligon <aligon90@gmail.com>
2021-09-07 17:13:38 -0500

    NTI-11143 expose the legacy context from Router

--- src/router/Context.jsx
+++ src/router/Context.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { ContextMerger } from './utils/context-merger';
-
 const Context = React.createContext();
 
-RouterContext.useRouter = () => {
-	return React.useContext(Context);
-};
-RouterContext.contextTypes = {
-	router: PropTypes.object,
-};
+export default class RouterContext extends React.Component {
+	static useRouter = () => {
+		return React.useContext(Context);
+	};
 
-export default function RouterContext({ children }, context) {
-	return (
-		<ContextMerger>
-			{router => (
-				<Context.Provider value={{ ...router, ...context?.router }}>
-					{children}
-				</Context.Provider>
-			)}
-		</ContextMerger>
-	);
+	static propTypes = {
+		children: PropTypes.any,
+	};
+
+	static contextTypes = {
+		router: PropTypes.object,
+	};
+
+	render() {
+		const { children } = this.props;
+		const { router } = this.context;
+
+		return <Context.Provider value={router}>{children}</Context.Provider>;
+	}
 }
